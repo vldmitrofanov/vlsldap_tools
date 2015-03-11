b5775f75d212558e9453d7af4ffeb808c8e33072
Vladimir Mitrofanov <vld.mitrofanov@gmail.com>
2015-03-11 12:14:07 -0400

    march 12 2015

--- lib/functions.sh
+++ lib/functions.sh
@@ -5,12 +5,18 @@ function generate_pass (){
         echo $MYSSWD
 }
 
+function generate_custom_pass (){
+        local MYSSWD=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w $1 | head -n 1)
+        echo $MYSSWD
+
+}
+
 function generate_sambaSID (){
 	sambaSID=
-        for num in 1 2 3 ;do
+        for num in 1;do
             randNum=$(od -vAn -N4 -tu4 < /dev/urandom | sed -e 's/ //g')
             if [ -z "$sambaSID" ];then
-                sambaSID="S-1-5-21-$randNum"
+                sambaSID="S-1-5-21-2636616856-561316741-165820116-$randNum"
             else
                 sambaSID="${sambaSID}-${randNum}"
             fi
